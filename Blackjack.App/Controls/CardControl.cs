0bb3da3ae304955f617d10f270a2cbaf5a6116eb
Vladislav Borovikov <vborovikov@live.ru>
2024-09-02 14:01:08 +0500

    added control properties

--- Blackjack.App/Controls/CardControl.cs
+++ Blackjack.App/Controls/CardControl.cs
@@ -18,18 +18,40 @@ public class CardControl : Control
     }
 
     public static readonly DependencyProperty ValueProperty =
-        DependencyProperty.Register(nameof(Value), typeof(Card?), typeof(CardControl), 
+        DependencyProperty.Register(nameof(Value), typeof(string), typeof(CardControl), 
             new PropertyMetadata(null, (d, e) => ((CardControl)d).OnValueChanged(e)));
 
+    internal static readonly DependencyPropertyKey SuitPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(Suit), typeof(CardSuit), typeof(CardControl),
+            new PropertyMetadata(CardSuit.Joker));
+
+    public static readonly DependencyProperty SuitProperty = SuitPropertyKey.DependencyProperty;
+
+    internal static readonly DependencyPropertyKey RankPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(Rank), typeof(CardRank), typeof(CardControl),
+            new PropertyMetadata(CardRank.None));
+
+    public static readonly DependencyProperty RankProperty = RankPropertyKey.DependencyProperty;
+
     [Bindable(true), Category("Content")]
-    public Card? Value
+    public string? Value
     {
-        get => (Card?)GetValue(ValueProperty);
+        get => GetValue(ValueProperty) as string;
         set => SetValue(ValueProperty, value);
     }
 
+    [Bindable(false), Browsable(false)]
+    public CardSuit Suit => (CardSuit)GetValue(SuitProperty);
+
+    [Bindable(false), Browsable(false)]
+    public CardRank Rank => (CardRank)GetValue(RankProperty);
+
     protected virtual void OnValueChanged(DependencyPropertyChangedEventArgs e)
     {
-
+        if (e.NewValue is string cardStr && Card.TryParse(cardStr, out var card))
+        {
+            SetValue(SuitPropertyKey, card.Suit);
+            SetValue(RankPropertyKey, card.Rank);
+        }
     }
 }
